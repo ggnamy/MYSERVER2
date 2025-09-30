26dd0a3c4ad2f7ab3f17030382d29bd0b4934b81
Anmanya <Anmanya.genius2549@gmail.com>
2025-09-30 16:14:37 +0700

    t

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Express = require("express");
-const app = Express();
-const port = 3000;
-app.get('/', (req, res) => 
-    res.send('Hello World!'));
-app.listen(3000, () => {
-    console.log(`Example app listening on port 3000 ${port}!` );
-});
+console.log('Hello World');
 //# sourceMappingURL=index.js.map
